57108f9b97dd25dbe12fed87bb450d4f0af205ea
DanielM <danieljhonsend99@gmail.com>
2020-12-24 00:16:38 -0500

    Burgers page

---
+++ main.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.container',{delay:200});
+ScrollReveal().reveal('.container-menu', {delay:400});
+ScrollReveal().reveal('.tel', {delay:400});
+ScrollReveal().reveal('.aside', {delay:700});
+ScrollReveal().reveal('.header', {delay:150});
+
